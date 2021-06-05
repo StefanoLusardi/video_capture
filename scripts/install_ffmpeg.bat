acd6055b42086d39aaf102fb7c81464dcfdc6b04
Stefano Lusardi <lusardi.stefano@gmail.com>
2021-06-05 23:11:12 +0200

    install_ffmpeg.bat (Windows) and CMakeLists

--- scripts/install_ffmpeg.bat
+++ scripts/install_ffmpeg.bat
@@ -1,18 +1,22 @@
 :: Download FFMPEG libraries
 echo "Install FFMPEG libraries"
-
 mkdir C:\ffmpeg\
 powershell -executionpolicy bypass -Command "& {$cli=New-Object System.Net.WebClient; $cli.DownloadFile('https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full-shared.7z','C:\ffmpeg\ffmpeg.7z')}"
 
-:: Unzip
+:: Unzip binaries and shared libraries
 set PATH=%PATH%;C:\Program Files\7-Zip\
+C:
 cd C:\ffmpeg
 7z x C:\ffmpeg\ffmpeg.7z -y
 
-:: Rename
-move ffmpeg-* ffmpeg
-del ffmpeg.7z
+:: Rename bin directory
+move C:\ffmpeg\ffmpeg-* C:\ffmpeg\ffmpeg
+del C:\ffmpeg\ffmpeg.7z
 
-:: Set PATH
+:: Set FFMPEG_DIR environment variable
 setx FFMPEG_DIR C:\ffmpeg\ffmpeg
+
+:: Check installation
 %FFMPEG_DIR%\bin\ffmpeg -version
+
+echo "FFMPEG libraries installed"
